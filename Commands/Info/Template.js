7666c0ef51fa6788de56ae811aaee5a717fb3048
Renixaus <94712193+Renixaus@users.noreply.github.com>
2022-07-21 13:47:49 +0300

    🦊🍓

---
+++ Commands/Info/Template.js
@@ -0,0 +1,12 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    name : '',  //Komut Adı
+    category : 'Info',  //Kategori
+    description : '',  //Açıklama
+    run : async(client, message, args) => {
+        
+        
+        
+    }
+}
